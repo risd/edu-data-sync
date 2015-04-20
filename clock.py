60faac59b2b2ba4d7c812c0e87c79610e514567e
rubillionaire <mail@rubenrodriguez.me>
2015-04-20 17:53:48 -0400

    Spacing
    Been a while since I've written python. spacing is significant.

--- clock.py
+++ clock.py
@@ -5,12 +5,12 @@ sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=60)
 def timed_job():
-	subprocess.call(['sync', 'events'])
+    subprocess.call(['sync', 'events'])
 
 
 @sched.scheduled_job('cron', day_of_week='sat', hour=6)
 def scheduled_job():
-	subprocess.call(['sync', 'employees'])
+    subprocess.call(['sync', 'employees'])
     subprocess.call(['sync', 'courses'])
 
 sched.start()
