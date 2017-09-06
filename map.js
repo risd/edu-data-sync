ba7fb46481041892e7251e6c3a14e7faaa2a46b4
rubonics <rubenrodriguez+github@gmail.com>
2017-09-05 20:44:02 -0400

    pushing the related content mapping framework
    + added functions for getting related content type key paths
    + added type annotations for turning related content key paths into data that can be mapped across, and subsequently saved.

--- map.js
+++ map.js
@@ -18,15 +18,28 @@ function MapCoordinator ( options, complete ) {
   if ( ! ( this instanceof MapCoordinator ) ) return new MapCoordinator( options, complete )
   var self = this;
 
+  // save reference for coordination
   this.mapPrototype = MapProtocol( options.mapPrototype )
   this.firebaseref = options.firebaseref;
 
+  // coordination
   this.isOneOff( function ( error, isOneOff ) {
     if ( error ) return complete( error )
 
+    // function used to apply the Map
     var applyMapFn = self.applyMapFn( isOneOff )
 
+    // DataToMap => MappedData
     self.applyMap( applyMapFn, complete )
+    
+    // relationship work in progress
+    // self.applyMap( applyMapFn, function ( error, mappedData ) {
+    //   self.relatedContentTypeKeyPaths( function ( error, relatedContentTypeKeyPaths ) {
+    //     debug( relatedContentTypeKeyPaths )
+    //     complete( error, relatedContentTypeKeyPaths )
+    //   } )
+    // } )
+
   } )
 
 }
@@ -89,6 +102,161 @@ MapCoordinator.prototype.applyMap = function ( applyMapFn, complete ) {
         complete( error, mappedSnapshotData )
       }
     } )
+
+}
+
+/**
+ * () => [ ContentTypeKeyPathControl ]
+ *
+ * ContentTypeKeyPathControl : { keyPath : ContentTypeKeyPath, control: ContentTypeControl }
+ * 
+ * @param  {Function} complete Callback
+ */
+MapCoordinator.prototype.relatedContentTypeKeyPaths = function ( complete ) {
+  var siteDataRef = this.firebaseref.child( 'data' )
+  var mappedContentType = this.mapPrototype.webhookContentType;
+
+  this.firebaseref.child( 'contentType' )
+    .once( 'value', contentTypeSnapshot, contentTypeError )
+
+  function contentTypeSnapshot ( snapshot ) {
+    var contentTypes = snapshot.val()
+    var relationships = relationshipsForContentType( mappedContentType, contentTypes )
+    var relationshipKeyPaths = relationships.map( keepKeys( 'keyPath' ) )
+    complete( null, relationshipKeyPaths )
+  }
+
+  function contentTypeError ( error ) {
+    complete( error )
+  }
+
+  function relationshipsForContentType ( relatedToContentType, contentTypes ) {
+    var isRelatedControl = function ( control ) {
+      return ( control.controlType === 'relation' && control.meta.contentTypeId === relatedToContentType )
+    }
+
+    var keysControls = contentTypeKeysControls( contentTypes )
+    var relationships = keysControls.filter( controlPasses( isRelatedControl ) )
+
+    return relationships;
+
+    function controlPasses ( predicate ) {
+      return function ( keyPathControlPair ) {
+        return predicate( keyPathControlPair.control )
+      }
+    }
+  }
+
+  /**
+   * ContentTypes => [ ContentTypeKeyPathControl ]
+   *
+   * ContentTypeKeyPath : [ type, widget ]
+   *  | [ type, widget, [], grid-key ]
+   *  | [ type, {}, widget ]
+   *  | [ type, {}, widget, [], grid-key ]
+   *
+   * ContentTypeControl : { name : string, controlType: string, controls : [ ContentTypeControl ]? }
+   *
+   * ContentTypeKeyPathControl : { keyPath : ContentTypeKeyPath, control: ContentTypeControl }
+   * 
+   * @param  {object} contentTypes  The webhook contentType object for the site
+   * @return {object} pairs         Array of ContentTypeKeyPathControl
+   */
+  function contentTypeKeysControls ( contentTypes ) {
+    var pairs = [];
+
+    function addPair ( keyPath, control ) {
+      pairs.push( {
+        keyPath: keyPath.slice( 0 ),
+        control: Object.assign( {}, control ),
+      } )
+    }
+
+    Object.keys( contentTypes ).forEach( function ( contentTypeKey ) {
+      var contentType = contentTypes[ contentTypeKey ]
+      var contentTypeKeyPath = [ contentTypeKey ]
+
+      if ( contentType.oneOff === false ) contentTypeKeyPath.push( {} )
+
+      contentType.controls.forEach( function ( control ) {
+        var keyPath = contentTypeKeyPath.concat( [ control.name ] )
+
+        if ( control.controlType === 'grid' ) {
+          keyPath.push( [] )
+
+          control.controls.forEach( function ( gridControl ) {
+            var gridControlKeyPath = keyPath.concat( [ gridControl.name ] )
+            addPair( gridControlKeyPath, gridControl )
+          } )
+        }
+        else {
+          addPair( keyPath, control )
+        }
+      } )
+    } )
+
+    return pairs;
+  }
+
+  function keepKeys ( keys ) {
+    if ( typeof keys === 'string' ) keys = [ keys ]
+    return function ( value ) {
+      var keep = {}
+      keys.forEach( function ( key ) { keep[ key ] = value[ key ] } )
+      return keep;
+    }
+  }
+}
+
+/**
+ * ( controlKeyPaths : [ { keyPath : ContentTypeKeyPath } ] ) => [ AbsoluteControlKeyPathData ]
+ *
+ * AbsoluteControlKeyPathData : { keyPath : ContentTypeKeyPath, control : ControlData }
+ *
+ *   Where `control` is the value of the `control-key` of the ContentTypeKeyPath
+ *
+ * ContentTypeKeyPath : [ type, control-key ]
+ *   | [ type, control-key, [], grid-key ]
+ *   | [ type, item-key, control-key ]
+ *   | [ type, item-key, control-key, [], grid-key ]
+ *
+ * ControlData : ( {} | [] | '' )
+ * 
+ * @param  {[type]} controlKeyPath
+ * @param  {[type]} complete       Function to call with 
+ */
+MapCoordinator.prototype.dataForContentTypeKeyPaths = function ( controlKeyPath, complete ) { }
+
+/**
+ * ( controlKeyPathDataArray : [ AbsoluteControlKeyPathData ], mappedData ) => ( mappedControlKeyPathData : [ RelativeControlKeyPathData ] )
+ *
+ * RelativeControlKeyPath : [ type, control-key ]
+ *   | [ type, item-key, control-key ]
+ *   
+ * ControlData : ( {} | [] | '' )
+ *
+ * RelativeControlKeyPathData : { keyPath: RelativeControlKeyPath, control: ControlData }
+ * 
+ * @param  {object} controlKeyPathDataArray  The AbsoluteControlKeyPathData's to apply the `mapRelatedExhibitionsFn` on.
+ * @param  {object} mappedData               The data that resulted from `applyMapFn`
+ * @return {object} mappedDataKeyPathPairs   The result of the appplication of `mapRelatedExhibitionsFn` on `controlKeyPathDataArray`
+ */
+MapCoordinator.prototype.applyMapRelatedFn = function ( controlKeyPathDataArray, mappedData ) { }
+
+/**
+ *
+ * controlKeyPathDataArray => controlKeyPathDataArray
+ * 
+ * controlKeyPathDataArray : [ RelativeControlKeyPathData ]
+ *
+ * `controlKeyPathDataArray` control's are saved to their keys.
+ * The callback `complete` is invoked with the same input data,
+ * `controlKeyPathDataArray`
+ * 
+ * @param  {object} controlKeyPathDataArray
+ * @param  {Function} complete              The function to call upon saving `controlKeyPathDataArray`
+ */
+MapCoordinator.prototype.saveControlKeyPathData = function ( controlKeyPathDataArray, complete ) {
 
 }
 
--- maps/exhibitions.js
+++ maps/exhibitions.js
@@ -1,4 +1,5 @@
 var debug = require( 'debug' )( 'map-exhibitions-prototype' )
+var assert = require( 'assert' )
 var moment = require( 'moment' )
 var timezone = require( 'moment-timezone' )
 
@@ -12,6 +13,7 @@ function MapExhibitions () {
 
 MapExhibitions.prototype.webhookContentType = 'communityexhibitions';
 MapExhibitions.prototype.mapFn = mapExhibitionsFn;
+MapExhibitions.prototype.mapRelatedFn = mapRelatedExhibitionsFn;
 
 function mapExhibitionsFn ( exhibition ) {
   // if now < end_date
@@ -19,7 +21,7 @@ function mapExhibitionsFn ( exhibition ) {
   //   set list_in_news_section to false
   //   set to isDraft to true
   if ( inThePast( exhibition.end_date ) ) {
-    exhibition.name = exhibition.name.startsWith( ARCHIVED_PREFIX )
+    exhibition.name = isArchivedExhibition( exhibition )
       ? exhibition.name
       : [ ARCHIVED_PREFIX, exhibition.name ].join( ' ' )
     exhibition.list_in_news_section = false;
@@ -37,4 +39,86 @@ function mapExhibitionsFn ( exhibition ) {
   function endOfDay ( timestamp ) {
     return [ timestamp.split( 'T' )[ 0 ], 'T23:59:59-04:00' ].join( '' )
   }
+}
+
+function mapRelatedExhibitionsFn ( widget, widgetKey, exhibitions ) {
+  assert( typeof exhibitions === 'object', 'Related data needs to be passed in to update relationship widgets.' )
+
+  if ( isRelationshipInRepeatable( widget, widgetKey ) ) {
+    // relationship widget lives in a repeatable
+    widget = widget.map( emptyRowIfWidgetKeyArchived ).filter( isPopulatedRow )
+  }
+  else if ( isMultipleRelationship( widget ) ) {
+    // widget is a multiple relationship
+    widget = widget.map( emptyRelationshipIfArchived ).filter( isPopulatedRelationship )
+  }
+  else if ( isSingleRelationship( widget ) ) {
+    // widget is a single relationship
+    widget = [ widget ].map( emptyRelationshipIfArchived )[ 0 ]
+  }
+
+  return widget;
+
+  function isRelationshipInRepeatable ( widget, widgetKey ) {
+    return Array.isArray( widget ) && typeof widgetKey === 'string'
+  }
+
+  function isMultipleRelationship ( widget ) {
+    return Array.isArray( widget )
+  }
+
+  function isSingleRelationship ( widget ) {
+    return typeof widget === 'string'
+  }
+
+  function emptyRelationshipIfArchived ( relationship ) {
+    if ( ! isPopulatedRelationship( relationship ) ) return relationship;
+    var relatedToKey = relationshipKey( relationship )
+    var relatedExhibition = exhibitions[ relatedToKey ]
+    if ( isArchivedExhibition( relatedExhibition ) ) relationship = '';
+    return relationship;
+  }
+
+  function relationshipKey ( relationship ) {
+    return relationship.split( ' ' )[ 1 ]
+  }
+
+  function isPopulatedRelationship ( relationship ) {
+    return typeof relationship === 'string' && relationship.split( ' ' ).length === 2;
+  }
+
+  function emptyRowIfWidgetKeyArchived ( row ) {
+    if ( ! isPopulatedRow( row ) ) return undefined;
+    
+    var rowWidget = row[ widgetKey ];
+    if ( ! rowWidget ) return undefined;
+
+    if ( isMultipleRelationship( rowWidget ) ) {
+      // no such cases, not sure how it should be handled yet
+      // var mappedRowWidget = rowWidget.map( emptyRelationshipIfArchived ).filter( isPopulatedRelationship )
+      // if ( differentRelationshipWidget( rowWidget, mappedRowWidget ) )
+    }
+    else if ( isSingleRelationship( rowWidget ) ) {
+      rowWidget = [ rowWidget ].map( emptyRelationshipIfArchived )[ 0 ]
+      if ( ! isPopulatedRelationship( mappedRowWidget ) ) row = undefined;
+    }
+
+    return row;
+  }
+
+  function isPopulatedRow ( row ) {
+    return typeof row === 'object';
+  }
+
+  function isPopulatedMultipleRelationship ( relationship ) {
+    return Array.isArray( relationship ) && relationship.length > 0;
+  }
+
+  function differentRelationshipWidget ( a, b ) {
+    return a.length !== b.length;
+  }
+}
+
+function isArchivedExhibition ( exhibition ) {
+  return typeof exhibition === 'object' && exhibition.name.startsWith( ARCHIVED_PREFIX )
 }
